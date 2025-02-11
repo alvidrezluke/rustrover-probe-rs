d2cfb5c98e82552ec1d253418058031840c6628c
GitHub Action <action@github.com>
2025-02-11 12:38:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/alvidrezluke/rustroverprobers/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alvidrezluke/rustroverprobers/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.alvidrezluke.rustroverprobers.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alvidrezluke.rustroverprobers.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
